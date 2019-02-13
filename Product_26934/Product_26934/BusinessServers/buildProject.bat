457072cea86c9f6b891650ee7ac26dbf4ddf4fa9
SKarimov <skarimov@ics.perm.ru>
2019-02-13 14:08:17 +0500

    initialize

---
+++ Product_26934/Product_26934/BusinessServers/buildProject.bat
@@ -0,0 +1,3 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+pushd "D:\SKarimov\Индивидуалка\CodeGen\Product_26934\Product_26934\BusinessServers"
+msbuild "D:\SKarimov\Индивидуалка\CodeGen\Product_26934\Product_26934\BusinessServers\Product_26934.BusinessServers.csproj"
---
+++ Product_26934/Product_26934/Objects/buildProject.bat
@@ -0,0 +1,3 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+pushd "D:\SKarimov\Индивидуалка\CodeGen\Product_26934\Product_26934\Objects"
+msbuild "D:\SKarimov\Индивидуалка\CodeGen\Product_26934\Product_26934\Objects\Product_26934.Objects.csproj"
