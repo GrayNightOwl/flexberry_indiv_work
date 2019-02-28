c8da673b251490bb9cf298438fc532201c13c317
SKarimov <skarimov@ics.perm.ru>
2019-02-28 15:51:43 +0500

    Попытка добавить тему BlueSky на экран логина

--- Product_26934/Product_26934/ASP.NET/LoginForm.aspx.cs
+++ Product_26934/Product_26934/ASP.NET/LoginForm.aspx.cs
@@ -1,11 +1,31 @@
 ﻿namespace ICSSoft.STORMNET.Web
 {
+    using System;
+    using System.Web;
+    using System.Web.Security;
+    using System.Web.UI;
+    using System.Web.UI.HtmlControls;
+
+    using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Web.AjaxControls;
     using ICSSoft.STORMNET.Web.AjaxControls.Forms;
+    using ICSSoft.STORMNET.Web.Tools;
+
+    using NewPlatform.Flexberry.Web.Themeable;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
 
     /// <summary>
     /// Class of user login form.
     /// </summary>
     public partial class LoginForm : BasePage
     {
+        protected override void OnInit(EventArgs e)
+        {
+
+            ThemeService.Current.Theme = "BlueSky";
+            base.OnInit(e);
+        }
     }
 }
