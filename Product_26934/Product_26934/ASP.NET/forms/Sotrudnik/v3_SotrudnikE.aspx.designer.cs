bb9fa6f951b4d5c7fdae546c752c2b09de6ae830
SKarimov <skarimov@ics.perm.ru>
2019-03-05 15:43:51 +0500

    Добавлена кнопка аудит

--- Product_26934/Product_26934/ASP.NET/forms/Sotrudnik/v3_SotrudnikE.aspx.designer.cs
+++ Product_26934/Product_26934/ASP.NET/forms/Sotrudnik/v3_SotrudnikE.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace IIS.Product_26934 {
     public partial class v3_СотрудникE {
         
         /// <summary>
+        /// AuditBtn элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton AuditBtn;
+        
+        /// <summary>
         /// ctrlФИОLabel элемент управления.
         /// </summary>
         /// <remarks>
